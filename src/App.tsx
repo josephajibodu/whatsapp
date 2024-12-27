8072b5c5be7bef8fc4062d3ee958a8275c3a726d
josephajibodu <josephajibodu@gmail.com>
2024-12-27 22:45:54 +0100

    deploy 3

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Joseph + Whatsapp</h1>
+      <h1>Joseph + Whatsapp - Deploy 3</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
